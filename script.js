f51382638fd3644d69d748a18763efe71966fa2b
PranithaVangala98 <108581187+PranithaVangala98@users.noreply.github.com>
2026-02-03 14:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 //complete this code
 class Person {
+	protected name: string;
+    protected age: number;
 	constructor(name:string, age: number){
 		this.name = name;
 		this.age = age;
 	}
 	get name(){
-		return name
+		return this.name
 	}
-	set age(age){
+	get age(): number {
+    return this.age;
+  }
+
+	set age(age : number){
 		this.age = age
+	}
+	set name(name:string){
+		this.name = name
 	}
 }
 
 class Student extends Person {
-	study(){
-		console.log(this.name+' is studying')
-	}
+	 study(): void {
+    console.log(`${this.name} is studying`);
+  }
 	
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(this.name+' is teaching')
-	}
+	 teach(): void {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
